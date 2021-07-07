d95576131e9c6bd4f3d81632fa59a48b4d26c22d
rara-token <raradev@protonmail.com>
2021-07-07 11:49:09 -0500

    update VotingMembershipListener to use ERC165 instead of `isVotingMembershipListener`

--- contracts/token/interfaces/IVotingMembershipListener.sol
+++ contracts/token/interfaces/IVotingMembershipListener.sol
@@ -1,7 +1,8 @@
-// An ITokenEmitter accumulates tokens on its own internal schedule,
-// allocating it to specific recipients.
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+// A listener for changes to Voting Membership levels, in case changes in those levels
+// carries a significance in accumulating effects (e.g. mining power).
 pragma solidity ^0.8.0;
-interface IVotingMembershipListener {
+interface IVotingMembershipListener is IERC165 {
     function membershipChanged(address _user, uint32 _prevLevel, uint32 _level) external;
-    function isVotingMembershipListener() external pure returns (bool);
 }
