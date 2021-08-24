cb9674b04d3c49d2eb89fb6c03deca6d467f007e
rara-token <raradev@protonmail.com>
2021-08-24 15:50:07 -0700

    include in previous

--- contracts/sales/blind/RaraAnimalGachaRack.sol
+++ contracts/sales/blind/RaraAnimalGachaRack.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.8.0;
 
-import "./presets/BlindCollectibleManagedFlowGachaRack.sol";
+import "./presets/BlindCollectibleManagedResupplyGachaRack.sol";
 
 // @notice A GachaRack for RaraAnim
-contract RaraAnimalGachaRack is BlindCollectibleManagedFlowGachaRack {
+contract RaraAnimalGachaRack is BlindCollectibleManagedResupplyGachaRack {
     constructor(address _prizeToken, address _purchaseToken, address _eip210, address _recipient)
-    BlindCollectibleManagedFlowGachaRack(_prizeToken, _purchaseToken, _eip210, _recipient) {
+    BlindCollectibleManagedResupplyGachaRack(_prizeToken, _purchaseToken, _eip210, _recipient) {
         // nothing else to do
     }
 }
